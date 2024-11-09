70ad35e6191d52d0822a1003f4a28652f1f0829e
Sricor <78024433+Sricor@users.noreply.github.com>
2024-11-09 17:11:30 +0800

    fix: opening too many files under weak network conditions (#33)

--- crates/client/bin/main.rs
+++ crates/client/bin/main.rs
@@ -64,7 +64,7 @@ struct Args {
     )]
     max_multiplex_interval: u64,
 
-    /// Connection multiplexing allowed within a specific interval 
+    /// Connection multiplexing allowed within a specific interval
     #[clap(
         long,
         help_heading = "Transport QUIC",
@@ -181,7 +181,10 @@ fn quic_config_from_args(args: &Args) -> Result<QuicConfig, Box<dyn Error>> {
         .with_initial_congestion_window(args.initial_congestion_window)
         .with_max_handshake_duration(Duration::from_millis(args.max_handshake_duration))
         .with_max_multiplex(args.max_multiplex)
-        .with_max_max_multiplex_interval((args.max_multiplex_per_interval, Duration::from_millis(args.max_multiplex_interval)))
+        .with_max_max_multiplex_interval((
+            args.max_multiplex_per_interval,
+            Duration::from_millis(args.max_multiplex_interval),
+        ))
         .with_max_idle_timeout(Duration::from_millis(args.max_idle_timeout))
         .with_max_keep_alive_period(Duration::from_millis(args.max_keep_alive_period))
         .with_max_open_bidirectional_streams(args.max_open_bidirectional_streams)
