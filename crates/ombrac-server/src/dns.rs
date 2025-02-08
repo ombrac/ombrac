9bb4d0f1548c57ae1b7d27ade502f227736c7e89
Sricor <78024433+Sricor@users.noreply.github.com>
2025-02-08 14:31:35 +0800

    fix: dependency conflict, temporarily remove hickory dns

--- crates/ombrac-server/src/dns.rs
+++ crates/ombrac-server/src/dns.rs
@@ -1,26 +1,14 @@
 use std::io;
-use std::net::IpAddr;
-use std::sync::LazyLock;
+use std::net::SocketAddr;
 
-use hickory_resolver::config::{ResolverConfig, ResolverOpts};
-use hickory_resolver::name_server::{GenericConnector, TokioRuntimeProvider};
-use hickory_resolver::{AsyncResolver, TokioAsyncResolver};
+pub(crate) async fn lookup_ip(host: &str) -> io::Result<SocketAddr> {
+    use tokio::net::lookup_host;
 
-static RESOLVER: LazyLock<AsyncResolver<GenericConnector<TokioRuntimeProvider>>> =
-    LazyLock::new(|| {
-        let config = ResolverConfig::google_h3();
-        let options = ResolverOpts::default();
-
-        TokioAsyncResolver::tokio(config, options)
-    });
-
-pub(crate) async fn lookup_ip(host: &str) -> io::Result<IpAddr> {
-    let response = RESOLVER
-        .lookup_ip(host)
+    let response = lookup_host(host)
         .await
         .map_err(|e| io::Error::other(e.to_string()))?;
 
-    response.iter().next().ok_or(io::Error::other(format!(
+    response.into_iter().next().ok_or(io::Error::other(format!(
         "failed to resolve domain {}",
         host
     )))
