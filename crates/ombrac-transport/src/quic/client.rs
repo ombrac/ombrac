263388669867e85e805c137365ea0b991a1b55db
Sricor <78024433+Sricor@users.noreply.github.com>
2025-02-15 22:56:41 +0800

    feat(client): disable cert verification for client

--- crates/ombrac-transport/src/quic/client.rs
+++ crates/ombrac-transport/src/quic/client.rs
@@ -12,6 +12,7 @@ pub struct Builder {
     server_address: String,
 
     tls_cert: Option<PathBuf>,
+    tls_skip: bool,
 
     enable_zero_rtt: bool,
     enable_connection_multiplexing: bool,
@@ -29,6 +30,7 @@ impl Builder {
             server_name: None,
             server_address,
             tls_cert: None,
+            tls_skip: false,
             enable_zero_rtt: false,
             enable_connection_multiplexing: false,
             congestion_initial_window: None,
@@ -50,6 +52,11 @@ impl Builder {
 
     pub fn with_tls_cert(mut self, value: PathBuf) -> Self {
         self.tls_cert = Some(value);
+        self
+    }
+
+    pub fn with_tls_skip(mut self, value: bool) -> Self {
+        self.tls_skip = value;
         self
     }
 
@@ -151,6 +158,12 @@ impl Connection {
                 .with_no_client_auth();
 
             tls_config.alpn_protocols = [b"h3"].iter().map(|&x| x.into()).collect();
+
+            if config.tls_skip {
+                tls_config
+                    .dangerous()
+                    .set_certificate_verifier(Arc::new(cert_verifier::NullVerifier));
+            }
 
             if config.enable_zero_rtt {
                 tls_config.enable_early_data = true;
@@ -256,5 +269,64 @@ impl Connection {
         });
 
         Ok(Connection(receiver))
+    }
+}
+
+mod cert_verifier {
+    use rustls::client::danger::{HandshakeSignatureValid, ServerCertVerified, ServerCertVerifier};
+    use rustls::pki_types::{CertificateDer, ServerName, UnixTime};
+    use rustls::Error;
+    use rustls::{DigitallySignedStruct, SignatureScheme};
+
+    #[derive(Debug)]
+    pub struct NullVerifier;
+
+    impl ServerCertVerifier for NullVerifier {
+        fn verify_server_cert(
+            &self,
+            _end_entity: &CertificateDer<'_>,
+            _intermediates: &[CertificateDer<'_>],
+            _server_name: &ServerName<'_>,
+            _ocsp_response: &[u8],
+            _now: UnixTime,
+        ) -> Result<ServerCertVerified, Error> {
+            Ok(ServerCertVerified::assertion())
+        }
+
+        fn verify_tls12_signature(
+            &self,
+            _message: &[u8],
+            _cert: &CertificateDer<'_>,
+            _dss: &DigitallySignedStruct,
+        ) -> Result<HandshakeSignatureValid, Error> {
+            Ok(HandshakeSignatureValid::assertion())
+        }
+
+        fn verify_tls13_signature(
+            &self,
+            _message: &[u8],
+            _cert: &CertificateDer<'_>,
+            _dss: &DigitallySignedStruct,
+        ) -> Result<HandshakeSignatureValid, Error> {
+            Ok(HandshakeSignatureValid::assertion())
+        }
+
+        fn supported_verify_schemes(&self) -> Vec<SignatureScheme> {
+            vec![
+                SignatureScheme::RSA_PKCS1_SHA1,
+                SignatureScheme::ECDSA_SHA1_Legacy,
+                SignatureScheme::RSA_PKCS1_SHA256,
+                SignatureScheme::ECDSA_NISTP256_SHA256,
+                SignatureScheme::RSA_PKCS1_SHA384,
+                SignatureScheme::ECDSA_NISTP384_SHA384,
+                SignatureScheme::RSA_PKCS1_SHA512,
+                SignatureScheme::ECDSA_NISTP521_SHA512,
+                SignatureScheme::RSA_PSS_SHA256,
+                SignatureScheme::RSA_PSS_SHA384,
+                SignatureScheme::RSA_PSS_SHA512,
+                SignatureScheme::ED25519,
+                SignatureScheme::ED448,
+            ]
+        }
     }
 }
